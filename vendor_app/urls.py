259c0bb565628a11c4ec87e90aee535509deeb72
ritin0204 <ritintiwari417@gmail.com>
2023-11-28 17:52:23 +0530

    completed the basic impplementation

--- vendor_app/urls.py
+++ vendor_app/urls.py
@@ -1,6 +1,24 @@
 from django.urls import path
-from . import views
+from .views import (
+    index,
+    VendorListCreateView,
+    VendorRetrieveUpdateDestroyView,
+    PurchaseOrderListCreateView,
+    PurchaseOrderRetrieveUpdateDestroyView,
+    VendorPerformanceView,
+)
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', index, name='index'),
+
+    # Vendor Profile Management
+    path('api/vendors/', VendorListCreateView.as_view(), name='create-vendor'),
+    path('api/vendors/<int:pk>/', VendorRetrieveUpdateDestroyView.as_view(), name='vendor-detail'),
+
+    # Purchase Order Tracking
+    path('api/purchase_orders/', PurchaseOrderListCreateView.as_view(), name='create-purchase-order'),
+    path('api/purchase_orders/<int:pk>/', PurchaseOrderRetrieveUpdateDestroyView.as_view(), name='purchase-order-detail'),
+
+    # Vendor Performance Evaluation
+    path('api/vendors/<int:pk>/performance/', VendorPerformanceView.as_view(), name='vendor-performance'),
 ]
--- vendor_app/views.py
+++ vendor_app/views.py
@@ -1,6 +1,39 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import generics, response
+from .models import Vendor, PurchaseOrder, HistoricalPerformance
+from .serializers import VendorSerializer, PurchaseOrderSerializer, HistoricalPerformanceSerializer
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Starting App..")
+    return HttpResponse("I am The Home Page :)")
+
+class VendorListCreateView(generics.ListCreateAPIView):
+    queryset = Vendor.objects.all()
+    serializer_class = VendorSerializer
+
+class VendorRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Vendor.objects.all()
+    serializer_class = VendorSerializer
+
+class PurchaseOrderListCreateView(generics.ListCreateAPIView):
+    queryset = PurchaseOrder.objects.all()
+    serializer_class = PurchaseOrderSerializer
+
+class PurchaseOrderRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = PurchaseOrder.objects.all()
+    serializer_class = PurchaseOrderSerializer
+
+class VendorPerformanceView(generics.RetrieveAPIView):
+    queryset = Vendor.objects.all()
+    serializer_class = VendorSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        performance_data = {
+            'on_time_delivery_rate': instance.on_time_delivery_rate,
+            'quality_rating': instance.quality_rating_avg,
+            'response_time': instance.average_response_time,
+            'fulfilment_rate': instance.fulfillment_rate,
+        }
+        return response.Response(performance_data)
